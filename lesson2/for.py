0cfce05c7cad9da0a5549cf25c4910ce96364915
Tamara Grigoryeva <naash7@gmail.com>
2017-09-11 00:52:33 +0300

    average for school fix

--- lesson2/for.py
+++ lesson2/for.py
@@ -4,16 +4,27 @@ for score in mylist:
 
 
 user_input=input("Введите любу строку: ") #task 2
-for characters in user_input:
-    print(characters)
+for character in user_input:
+    print(character)
 
 
-list_of_pupils=[ {'school_class': "4a", 'scores': [5,5,2,3]}, {'school_class': "4b", 'scores': [2,2,2,3]}, {'school_class': "4c", 'scores': [3,5,5,3]} ] #task 3
+#task 3
+list_of_pupils=[
+{'school_class': "4a", 'scores': [5,5,2,3,5]}, 
+{'school_class': "4b", 'scores': [2,2,3]}, 
+{'school_class': "4c", 'scores': [3,5,5,3]} 
+]
+
 total_score_school = 0
-for dictionary in list_of_pupils:
-    average_class = sum(dictionary["scores"])/len(dictionary["scores"])
-    total_score_school += average_class
-    print(dictionary["school_class"],"-",average_class)
-average_school = total_score_school/len(list_of_pupils)
+iterrator = 0
 
-print("Average score: ",average_school)
+for one_class in list_of_pupils:
+    average_class = round(sum(one_class["scores"])/len(one_class["scores"]),2)
+    print("Средний балл в классе",one_class["school_class"],"-",average_class)
+    for pupils in one_class["scores"]:
+        total_score_school += pupils
+        iterrator += 1
+        print(pupils)
+
+average_school = round(total_score_school/iterrator,2)
+print("Средний балл по школе",average_school)
