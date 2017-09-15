a6eec1fd306cc2bca75d05a4f05896da55f3788b
Tamara Grigoryeva <naash7@gmail.com>
2017-09-15 23:13:25 +0300

    name corr

---
+++ lesson2/next_full_moon_for_bot.py
@@ -0,0 +1,20 @@
+'''
+Полнолуние
+Научить бота отвечать на вопрос “Когда ближайшее полнолуние после 2016-10-01?”. 
+Чтобы узнать, когда ближайшее полнолуние, используй модуль ephem. 
+Чтобы им пользоваться, его нужно установить ($ pip install ephem) и импортировать. 
+'''
+import ephem
+import re
+from datetime import datetime, date
+
+when_next_full_moon_question = "Когда ближайшее полнолуние после 2016-10-01"
+
+find_date_in_next_full_moon_question = re.findall(r'\d{4}-\d{2}-\d{2}' , when_next_full_moon_question)
+find_date_in_next_full_moon_question_String = ''.join(find_date_in_next_full_moon_question).replace("-","/")
+
+
+next_full_moon_date = datetime.strptime(find_date_in_next_full_moon_question_String, "%Y/%d/%m")
+print(ephem.next_full_moon(next_full_moon_date))
+
+
